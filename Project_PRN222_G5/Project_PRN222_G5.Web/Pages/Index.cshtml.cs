a5a137d0609c86585d166310aa755237c09ee45c
dullahan <dangdeptrai1212@gmail.com>
2025-05-12 22:48:02 +0700

    feat: Structure Project

---
+++ Project_PRN222_G5/Project_PRN222_G5.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_PRN222_G5.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
