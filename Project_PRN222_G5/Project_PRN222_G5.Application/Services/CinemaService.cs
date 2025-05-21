fabf0bb6dddd49e637f593d119329b2e8624971d
Shile <dangvtn2004@gmail.com>
2025-05-21 19:44:04 +0700

    .

--- Project_PRN222_G5/Project_PRN222_G5.Application/Services/CinemaService.cs
+++ Project_PRN222_G5/Project_PRN222_G5.Application/Services/CinemaService.cs
@@ -3,7 +3,7 @@ using Project_PRN222_G5.Application.DTOs.Cinema.Response;
 using Project_PRN222_G5.Application.Interfaces.Service;
 using Project_PRN222_G5.Application.Mapper.Cinema;
 using Project_PRN222_G5.Domain.Entities.Cinema;
-using Project_PRN222_G5.Domain.Interfaces;
+
 using System.ComponentModel.DataAnnotations;
 
 namespace Project_PRN222_G5.Application.Services;
