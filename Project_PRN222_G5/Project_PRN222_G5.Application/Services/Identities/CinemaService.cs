b29895c0bc3281baf77cf99a720272a5d39d40d1
dullahan <dangdeptrai1212@gmail.com>
2025-05-21 22:09:04 +0700

    fix: add validationService

--- Project_PRN222_G5/Project_PRN222_G5.Application/Services/Identities/CinemaService.cs
+++ Project_PRN222_G5/Project_PRN222_G5.Application/Services/Identities/CinemaService.cs
@@ -2,13 +2,16 @@
 using Project_PRN222_G5.Application.DTOs.Cinema.Response;
 using Project_PRN222_G5.Application.Interfaces.Service.Identities;
 using Project_PRN222_G5.Application.Interfaces.UnitOfWork;
+using Project_PRN222_G5.Application.Interfaces.Validation;
 using Project_PRN222_G5.Application.Mapper.Cinema;
 using Project_PRN222_G5.Domain.Entities.Cinema;
 
 namespace Project_PRN222_G5.Application.Services.Identities;
 
-public class CinemaService(IUnitOfWork unitOfWork)
-    : GenericService<Cinema, CreateCinemaDto, UpdateCinemaDto, CinemaResponse>(unitOfWork), ICinemaService
+public class CinemaService(
+    IUnitOfWork unitOfWork,
+    IValidationService validationService
+    ) : GenericService<Cinema, CreateCinemaDto, UpdateCinemaDto, CinemaResponse>(unitOfWork, validationService), ICinemaService
 {
     protected override CinemaResponse MapToResponse(Cinema entity) => entity.ToCinemaResponse();
 
