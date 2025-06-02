8eb9d9bf0e7b4df86ee47412289fabff1129443e
dullahan <dangdeptrai1212@gmail.com>
2025-06-02 20:39:37 +0700

    chore: add retry connect db

--- Project_PRN222_G5/Project_PRN222_G5.Web/DependencyInjection.cs
+++ Project_PRN222_G5/Project_PRN222_G5.Web/DependencyInjection.cs
@@ -47,7 +47,11 @@ public static class DependencyInjection
         #region DbContext
 
         services.AddDbContext<TheDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                sqlOptions => sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorNumbersToAdd: null)));
         services.AddScoped<IDbContext, TheDbContext>();
 
         #endregion DbContext
