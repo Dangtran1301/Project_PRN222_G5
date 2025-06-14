b596c151b7802012b3562cc9e96e6e0b8e684984
Shile <dangvtn2004@gmail.com>
2025-06-14 13:39:55 +0700

    Add view cinema for user MVC

---
+++ Project_PRN222_G5/Project_PRN222_G5.Web/Controllers/CinemasController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project_PRN222_G5.BusinessLogic.DTOs;
+using Project_PRN222_G5.BusinessLogic.DTOs.Cinema.Response;
+using Project_PRN222_G5.BusinessLogic.Interfaces.Service.Cinema;
+
+namespace Project_PRN222_G5.Web.Controllers
+{
+    [AllowAnonymous]
+    public class CinemasController : Controller
+    {
+        private readonly ICinemaService _cinemaService;
+
+        public CinemasController(ICinemaService cinemaService)
+        {
+            _cinemaService = cinemaService;
+        }
+
+        // GET: Cinemas
+        public async Task<IActionResult> Index(string? search)
+        {
+            var request = new PagedRequest
+            {
+                PageNumber = 1,
+                PageSize = 100,
+                Search = search
+            };
+
+            var result = await _cinemaService.GetPagedAsync(request);
+            ViewBag.Search = search;
+            return View(result.Data);
+        }
+
+        // GET: Cinemas/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null) return NotFound();
+
+            var cinema = await _cinemaService.GetByIdAsync(id.Value);
+            if (cinema == null) return NotFound();
+
+            return View(cinema);
+        }
+    }
+}
